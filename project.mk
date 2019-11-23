f0c2b5ef9bf2fcbc7338cfe9503b833f779039f0
Charlie Bruce <charliebruce@gmail.com>
2019-11-23 22:59:57 +0000

    Update bootloader version

--- project.mk
+++ project.mk
@@ -6,7 +6,7 @@ ARTEFACTS_DIR = artefacts
 
 SDK_ROOT=/nrf5/nRF5_SDK_15.2.0
 APPLICATION = app/src/pca10040/s132/armgcc/_build_$(COMBINED_PRODUCT_HARDWARE_VERSION)/nrf52832_xxaa.hex
-BOOTLOADER = boot_nrf52/bootloader_secure/pca10040_ble/armgcc/_build_$(COMBINED_PRODUCT_HARDWARE_VERSION)/nrf52832_xxaa_s132.hex
+BOOTLOADER = boot/bootloader_secure/pca10040_ble/armgcc/_build_$(COMBINED_PRODUCT_HARDWARE_VERSION)/nrf52832_xxaa_s132.hex
 SOFTDEVICE = $(SDK_ROOT)/components/softdevice/s132/hex/s132_nrf52_6.1.0_softdevice.hex
 BL_SETTINGS = $(TEMP_DIR)/bootloadersettings.hex
 
@@ -25,10 +25,13 @@ all:
 	
 	# Build the sub-projects
 	make SDK_ROOT=$(SDK_ROOT) OUTPUT_DIRECTORY=_build_$(COMBINED_PRODUCT_HARDWARE_VERSION) -C app/src/pca10040/s132/armgcc
-	make SDK_ROOT=$(SDK_ROOT) OUTPUT_DIRECTORY=_build_$(COMBINED_PRODUCT_HARDWARE_VERSION) -C boot_nrf52/bootloader_secure/pca10040_ble/armgcc
+	make SDK_ROOT=$(SDK_ROOT) OUTPUT_DIRECTORY=_build_$(COMBINED_PRODUCT_HARDWARE_VERSION) -C boot/bootloader_secure/pca10040_ble/armgcc
+	
+	# Make the artefacts directory if it does not exist
+	mkdir -p artefacts
 	
 	# Build the DFU package from the app
-	nrfutil pkg generate --hw-version $(COMBINED_PRODUCT_HARDWARE_VERSION) --application-version 1 --application $(OUTPUT_DIRECTORY)/nrf52832_xxaa.hex --sd-req 0xAF --key-file $(PROJ_DIR)/private.pem $(ARTEFACTS_DIR)/dfu-$(VERSION_IDENTIFIER).zip
+	nrfutil pkg generate --hw-version $(COMBINED_PRODUCT_HARDWARE_VERSION) --application-version 1 --application $(APPLICATION) --sd-req 0xAF --key-file private.pem $(ARTEFACTS_DIR)/dfu-$(VERSION_IDENTIFIER).zip
 	
 	# Merge hex files to form a complete package
 	# In order to boot into the app immediately after flashing, the bootloader's settings page needs to be written
@@ -42,6 +45,6 @@ clean:
 	@echo "project.mk is cleaning for $(PRODUCT_NAME) with PRODUCT_ID=$(PRODUCT_ID), PCBVER=$(PCBVER)"
 	rm -rf hex/
 	make SDK_ROOT=$(SDK_ROOT) OUTPUT_DIRECTORY=_build_$(COMBINED_PRODUCT_HARDWARE_VERSION) -C app/src/pca10040/s132/armgcc clean
-	make SDK_ROOT=$(SDK_ROOT) OUTPUT_DIRECTORY=_build_$(COMBINED_PRODUCT_HARDWARE_VERSION) -C boot_nrf52/bootloader_secure/pca10040_ble/armgcc clean
+	make SDK_ROOT=$(SDK_ROOT) OUTPUT_DIRECTORY=_build_$(COMBINED_PRODUCT_HARDWARE_VERSION) -C boot/bootloader_secure/pca10040_ble/armgcc clean
 	rm $(ARTEFACTS_DIR)/img-$(VERSION_IDENTIFIER).hex
 	rm $(ARTEFACTS_DIR)/dfu-$(VERSION_IDENTIFIER).zip
